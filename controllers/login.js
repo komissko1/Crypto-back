c9a365e2538d3ea45327d286a8b437b0944b71d2
Konstantin Komisarov <komissko1@yandex.ru>
2023-02-28 13:50:01 +0100

    Update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -27,7 +27,7 @@ module.exports.login = (req, res, next) => {
             maxAge: 7200000,
             httpOnly: true,
             secure: true,
-            SameSite: None,
+            sameSite: 'none',
           })
           .status(200)
           .send({
